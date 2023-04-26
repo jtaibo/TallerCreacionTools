cd2cf5ee68c8a77326c60049b0e3a61b0f24737c
Ángel Formoso Caamaño <119257115+legno9@users.noreply.github.com>
2023-04-26 12:45:15 +0200

    Master of checkers splitted

--- maya/python/tlc/common/checkers/masterofcheckers.py
+++ maya/python/tlc/common/checkers/masterofcheckers.py
@@ -5,17 +5,29 @@ from importlib import reload
 def start(): #Función "start" a la que cambie el nombre
     file_name = cmds.file( query=True, sn=True, shn=True)
     file_name = file_name.split("_")
+<<<<<<< HEAD
     check = ["pipeline","naming","shading"]
+=======
+    check = ["Pipeline","Naming"]
+>>>>>>> 9915305 (Master of checkers splitted)
 
     try:
         taskID = file_name[2]
         if taskID == "mlp" or taskID == "mhp" or taskID == "msc" or taskID == "bls":
+<<<<<<< HEAD
             check.append("modeling")
         if taskID == "anim" or taskID == "layout":
             check.append("rigging")
     except:
         # print("Invalid scene name")
         pass
+=======
+            check.append("Modeling")
+        if taskID == "anim" or taskID == "layout":
+            check.append("Rigging")
+    except:
+        print("Invalid scene name")
+>>>>>>> 9915305 (Master of checkers splitted)
 
     reload (master_ui)
     master_ui.run(check)
